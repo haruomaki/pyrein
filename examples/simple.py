5ad7367b5277e68277ed254a5c27da1ca4ba1aa8
Haruo <haruo-mtok@outlook.com>
2026-01-10 23:14:18 +0900

    Envクラス作成

--- examples/simple.py
+++ examples/simple.py
@@ -6,11 +6,15 @@ def simulate(state: int, input: int) -> int:
 
 
 def render(prev: int, curr: int, dt: float):
-    print(f"[render] 現在の状態は{curr}です。")
+    print(f"[render] 現在の状態は{prev}です。")
 
 
 def input_provider() -> int:
     return 5
 
 
-pyrein.run_game(0, simulate, render, input_provider)
+env = pyrein.Env[int, int]()
+env.simulate = simulate
+env.render = render
+env.input_provider = input_provider
+env.run_game(0)
--- src/pyrein/__init__.py
+++ src/pyrein/__init__.py
@@ -3,22 +3,24 @@ from typing import TypeVar, Callable
 S = TypeVar("S")  # State
 M = TypeVar("M")  # Message / Input
 
-
 Simulator = Callable[[S, M], S]
 Renderer = Callable[[S, S, float], None]
 InputProvider = Callable[[], M]
 
 
-def run_game(
-    initial_state: S,
-    simulate: Simulator[S, M],
-    render: Renderer[S],
-    input_provider: InputProvider[M],
-) -> None:
-    state = initial_state
+class Env[S, M]:
+    simulate: Simulator[S, M]
+    render: Renderer[S]
+    input_provider: InputProvider[M]
 
-    for t in range(10):
-        msg = input_provider()
-        next_state = simulate(state, msg)
-        render(state, next_state, float(t))
-        state = next_state
+    def run_game(
+        self,
+        initial_state: S,
+    ) -> None:
+        state = initial_state
+
+        for t in range(10):
+            msg = self.input_provider()
+            next_state = self.simulate(state, msg)
+            self.render(state, next_state, float(t))
+            state = next_state
