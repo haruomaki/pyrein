a6ba724abba5d36125b90141bc675a5a8bca27e2
Haruo <haruo-mtok@outlook.com>
2026-01-10 19:21:00 +0900

    TypeVarを使って原型作り

--- examples/simple.py
+++ examples/simple.py
@@ -1,3 +1,12 @@
 import pyrein
 
-pyrein.hello()
+
+def simulate(state: int, message: int) -> int:
+    return state + message
+
+
+def render(current: int, next: int, time: float):
+    print(f"[render] 現在の状態は{current}です。")
+
+
+pyrein.run_game(0, simulate, render, 5)
--- src/pyrein/__init__.py
+++ src/pyrein/__init__.py
@@ -1,2 +1,22 @@
 def hello():
     print("こんにちは！")
+
+
+from typing import TypeVar
+
+S = TypeVar("S")  # State
+M = TypeVar("M")  # Message / Input
+
+from typing import Callable
+
+SimulateFn = Callable[[S, M], S]
+RenderFn = Callable[[S, S, float], None]
+
+
+def run_game(initial_state: S, simulate: SimulateFn, render: RenderFn, dummy_imput: M):
+    current_state = initial_state
+
+    for t in range(10):
+        next_state = simulate(current_state, dummy_imput)
+        render(current_state, next_state, t)
+        current_state = next_state
