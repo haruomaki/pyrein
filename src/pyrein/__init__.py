13c1418eace09374ca029ae924c1ebf27eb448a8
Haruo <haruo-mtok@outlook.com>
2026-01-11 23:51:08 +0900

    _simulation_step関数廃止

--- src/pyrein/__init__.py
+++ src/pyrein/__init__.py
@@ -26,60 +26,51 @@ dt = 1.0
 elapsed = 0.0
 
 
-def _simulation_step[S, M](
-    simulate: Callable[[S, M], S],
-    render: Callable[[S, S], Iterator[None]],
-    input_provider: Callable[[], M],
-) -> bool:
-    global _prev, _curr, elapsed
-    # 規定の時間が経過するまで描画ループ
-    simstart = datetime.now()
-    it = render(_prev, _curr)
-    elapsed = 0.0  # 最後に状態が更新されてからの経過時間（秒）
-    while elapsed < dt:
-        # 経過時間の計算
-        now = datetime.now()
-        elapsed = (now - simstart).total_seconds()
-
-        # イベント処理
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
-
-        # 画面を黒でクリア
-        screen.fill(BLACK)
-
-        # 描画
-        next(it)
-
-        # 描画更新
-        pygame.display.flip()
-
-        # フレームレート維持
-        clock.tick(FPS)
-
-    # 時間が来たらゲーム世界を進める
-    msg = input_provider()
-    next_state = simulate(_curr, msg)
-    _prev = _curr
-    _curr = next_state
-
-    return True
-
-
 def run[S, M](
     simulate: Callable[[S, M], S],
     render: Callable[[S, S], Iterator[None]],
     input_provider: Callable[[], M],
     initial_state: S,
 ) -> None:
-    global _prev, _curr
+    global _prev, _curr, elapsed
     print("run_gameはじめ")
     _prev = initial_state
     _curr = initial_state
 
-    while _simulation_step(simulate, render, input_provider):
-        pass
+    # シミュレーションループ
+    running = True
+    while running:
+        # 規定の時間が経過するまで描画ループ
+        simstart = datetime.now()
+        it = render(_prev, _curr)
+        elapsed = 0.0  # 最後に状態が更新されてからの経過時間（秒）
+        while running and elapsed < dt:
+            # 経過時間の計算
+            now = datetime.now()
+            elapsed = (now - simstart).total_seconds()
+
+            # イベント処理
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            # 画面を黒でクリア
+            screen.fill(BLACK)
+
+            # 描画
+            next(it)
+
+            # 描画更新
+            pygame.display.flip()
+
+            # フレームレート維持
+            clock.tick(FPS)
+
+        # 時間が来たらゲーム世界を進める
+        msg = input_provider()
+        next_state = simulate(_curr, msg)
+        _prev = _curr
+        _curr = next_state
 
     # Pygameの終了
     pygame.quit()
