67e6d440e2a67a7274f64176124da4fc6da769a7
Haruo <haruo-mtok@outlook.com>
2026-01-13 21:45:16 +0900

    上下左右に移動

--- examples/simple.py
+++ examples/simple.py
@@ -1,32 +1,51 @@
 import pyrein
 import pygame
 from pyrein.easing import ease_out
+from dataclasses import dataclass
 
 
-def simulate(state: int, msg: int) -> int:
-    # print(f"[simulate] {state=}")
-    return state + msg
+@dataclass
+class State:
+    x: int
+    y: int
 
 
-def render(prev: int, curr: int):
+Action = int | None
+
+
+def simulate(state: State, action: Action) -> State:
+    if action is None:
+        return state
+    dx = [0, 0, -1, 1][action]
+    dy = [-1, 1, 0, 0][action]
+    return State(state.x + dx, state.y + dy)
+
+
+def render(prev: State, curr: State):
     print(f"[render] 現在の状態は{curr}です。")
     while True:
         # 円を描画
-        x = pyrein.lerp(200 + prev * 10, 200 + curr * 10, ease_out(0.5))
-        pygame.draw.circle(pyrein.screen, (23, 200, 100), (x, 200), 80)
+        x = pyrein.lerp(200 + prev.x * 30, 200 + curr.x * 30, ease_out(0.5))
+        y = pyrein.lerp(200 + prev.y * 30, 200 + curr.y * 30, ease_out(0.5))
+        pygame.draw.circle(pyrein.screen, (23, 200, 100), (x, y), 80)
         yield
 
 
 def decide():
+    action: Action = None
     while pyrein.elapsed < 1:
         keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            action = 0
+        if keys[pygame.K_DOWN]:
+            action = 1
         if keys[pygame.K_LEFT]:
-            print("<-")
+            action = 2
         if keys[pygame.K_RIGHT]:
-            print("->")
+            action = 3
         yield
 
-    return 10
+    return action
 
 
-pyrein.run(simulate, decide, render, 0)
+pyrein.run(simulate, decide, render, State(0, 0))
