d777ee2446aa208bf9a015fd1fb2ff4b104fd9b3
Haruo <haruo-mtok@outlook.com>
2026-01-16 00:05:19 +0900

    蛇の体を長く🐍

--- examples/snake.py
+++ examples/snake.py
@@ -1,5 +1,6 @@
 import pyrein
 import pygame
+from pygame import Vector2 as Vec2
 from pyrein.easing import ease_out
 from dataclasses import dataclass
 
@@ -19,8 +20,8 @@ GRID_HEIGHT = 10
 @dataclass
 class State:
     direction: int
-    x: int
-    y: int
+    xs: list[int]
+    ys: list[int]
 
 
 Action = int | None
@@ -34,8 +35,11 @@ def simulate(state: State, action: Action) -> State:
     BACK = [(0, 1), (1, 0), (2, 3), (3, 2)]
     if action is not None and not (state.direction, action) in BACK:
         state.direction = action
-    state.x += [0, 0, -1, 1][state.direction]
-    state.y += [-1, 1, 0, 0][state.direction]
+    new_x = state.xs[0] + [0, 0, -1, 1][state.direction]
+    new_y = state.ys[0] + [-1, 1, 0, 0][state.direction]
+    state.xs = [new_x] + state.xs[0:-1]
+    state.ys = [new_y] + state.ys[0:-1]
+    print(state.xs)
     return state
 
 
@@ -59,9 +63,12 @@ def render(prev: State, curr: State):
     while True:
         draw_grid()
         # 円を描画
-        x = pyrein.lerp(prev.x * GRID_SIZE, curr.x * GRID_SIZE, ease_out(dt, 1.5))
-        y = pyrein.lerp(prev.y * GRID_SIZE, curr.y * GRID_SIZE, ease_out(dt, 1.5))
-        pyrein.draw.circle(SNAKE_HEAD, (x, y), GRID_SIZE / 2)
+        for i in range(len(curr.xs)):
+            pr = Vec2(prev.xs[i], prev.ys[i])
+            cr = Vec2(curr.xs[i], curr.ys[i])
+            x = pyrein.lerp(pr.x * GRID_SIZE, cr.x * GRID_SIZE, ease_out(dt, 1.5))
+            y = pyrein.lerp(pr.y * GRID_SIZE, cr.y * GRID_SIZE, ease_out(dt, 1.5))
+            pyrein.draw.circle(SNAKE_HEAD, (x, y), GRID_SIZE / 2)
         yield
 
 
@@ -82,4 +89,4 @@ def decide():
     return action
 
 
-pyrein.run(simulate, decide, render, State(1, 0, 0))
+pyrein.run(simulate, decide, render, State(1, [0, 1, 2, 3, 4], [0, 0, 0, 0, 0]))
